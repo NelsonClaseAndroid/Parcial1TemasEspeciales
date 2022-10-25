5bf31763ef0ca2d1f48749a3a712388a4d874253
NDavidDuranM <20171056@ce.pucmm.edu.do>
2022-10-24 20:44:31 -0400

    Third commit

--- app/src/main/java/com/pucmm/a1erparcialtemasespeciales/MainActivity.java
+++ app/src/main/java/com/pucmm/a1erparcialtemasespeciales/MainActivity.java
@@ -1,16 +1,22 @@
 package com.pucmm.a1erparcialtemasespeciales;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.pucmm.a1erparcialtemasespeciales.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    List<Product> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,16 @@ public class MainActivity extends AppCompatActivity {
         Button eliminar1;
         Button eliminar2;
         Button eliminar3;
+
+        int spanCount = 1;
+
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 2;
+        }
+
+        recyclerView1.setHasFixedSize(true);
+        recyclerView1.setLayoutManager(new GridLayoutManager(getApplicationContext(), spanCount));
+
+        //recyclerView1.setAdapter();
     }
 }
